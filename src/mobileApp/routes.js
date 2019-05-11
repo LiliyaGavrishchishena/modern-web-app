58de2016ebcf6ff30b82793bcf80713a5e590569
Liliya Gavrishchishena <Liliya.Gavrishchishena@gmail.com>
2019-05-11 14:51:51 +0300

    mobile

---
+++ src/mobileApp/routes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { serverFetch as homeFetch } from '&/redux/serverFetch';
+
+import LoadableHOC from '&/LoadableHOC';
+const AsyncHome = LoadableHOC({
+  loader: () => import(/* webpackChunkName: "MobileHome" */ './Home')
+});
+const AsyncAbout = LoadableHOC({
+  loader: () => import(/* webpackChunkName: "MobileAbout" */ './About')
+});
+
+export default [
+  {
+    path: '/',
+    component: AsyncHome,
+    serverFetch: homeFetch,
+    exact: true
+  },
+  {
+    path: '/about',
+    component: AsyncAbout,
+    exact: true
+  }
+];
